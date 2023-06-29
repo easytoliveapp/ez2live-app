5c367529f8f05e2d3a852542e224e9c11293d9a1
Pedro Lima <pedro@deonibus.com>
2023-06-29 19:15:39 -0300

    feat: Initial commit - start boilerplat

---
+++ src/app/heroicons-solid.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import HeroiconsSolid from "@heroicons/react/24/solid";
+
+export default HeroiconsSolid;
+
+
+// app/rotas...
+// app/supplier/rotas...
+// app/supplier <- layout diferente
