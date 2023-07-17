55ee56f0318c1c1c29e2aaf764887af95d1e121a
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-07-17 09:51:17 -0300

    feat: address typologi

--- src/types/auth/index.ts
+++ src/types/auth/index.ts
@@ -1,13 +1,21 @@
 export interface ILogIn {
   email: string;
   password: string;
-};
+}
 
+interface IAddress {
+  street: string;
+  number: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
 export interface IRegisterAccount {
   email: string;
   password: string;
   name: string;
-  isSupplier: boolean;
-  address: {};
+  isSupplier?: boolean;
+  address: IAddress;
   document?: string;
-};
+}
