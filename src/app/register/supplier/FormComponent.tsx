b6539dcee79826b001621c423c36bffff40163ab
Pedro Lima <pedro@deonibus.com>
2023-07-25 20:54:10 -0300

    fix input error to use formik

--- src/app/register/supplier/FormComponent.tsx
+++ src/app/register/supplier/FormComponent.tsx
@@ -5,93 +5,99 @@ import { Formik, Form, Field } from "formik";
 import { Input, ButtonPrimary } from "@/components/atoms";
 import * as Yup from "yup";
 
-const SignupSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  document: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string()
-    .matches(
-      /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/,
-      "Must containt at least 1 letter and 1 number"
-    )
-    .required(
-      "Enter a combination of at least eight numbers,letters and punctuation marks(such as ! and &)."
-    )
-    .min(8, "Password must be atleast 8 characters.")
-    .max(36, "Password can't be more than 36 characters"),
-  conf_password: Yup.string()
-    .required("Confirm your password.")
-    .oneOf([Yup.ref("password")], "Passwords must match."),
-});
-const FormComponent = () => (
-  <div>
+const FormComponent = () => {
+  const SignupValidationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    document: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string()
+      .matches(
+        /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]/,
+        "Must containt at least 1 letter and 1 number"
+      )
+      .required(
+        "Enter a combination of at least eight numbers,letters and punctuation marks(such as ! and &)."
+      )
+      .min(8, "Password must be atleast 8 characters.")
+      .max(36, "Password can't be more than 36 characters"),
+    conf_password: Yup.string()
+      .required("Confirm your password.")
+      .oneOf([Yup.ref("password")], "Passwords must match."),
+  });
+
+  const initialValues: any = {
+    name: "",
+    document: "",
+    email: "",
+    password: "",
+    conf_password: "",
+  };
+
+  const handleFormSubmit = (values: any) => {
+    console.log("Usuario registrado", values);
+  }
+
+  return (
     <Formik
-      initialValues={{
-        name: "",
-        document: "",
-        email: "",
-        password: "",
-        conf_password: "",
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={(values) => {
-        // same shape as initial values
-        console.log(values);
-      }}
+      initialValues={initialValues}
+      validationSchema={SignupValidationSchema}
+      onSubmit={handleFormSubmit}
     >
-      {({ errors, touched }) => (
-        <Form className="grid grid-cols-1 gap-6">
-          <label className="block" htmlFor="name">
-            <span className="text-neutral-800 dark:text-neutral-200">Name</span>
-            <Field name="name" />
-            {errors.name && touched.name ? <div>{errors.name}</div> : null}
-          </label>
-
-          <label htmlFor="document" className='block'>
-            <span className="text-neutral-800 dark:text-neutral-200">Document</span>
-          <Field name="document" />
-          {errors.document && touched.document ? (
-            <div>{errors.document}</div>
-          ) : null}
-          </label>
-
-          <label htmlFor="email" className='block'>
-            <span className="text-neutral-800 dark:text-neutral-200">Email</span>
-          <Field name="email" type="email" />
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          </label>
-
-          <label htmlFor="password" className='block'>
-            <span className="text-neutral-800 dark:text-neutral-200">Password</span>
-          <Field name="password" type="password" />
-          {errors.password && touched.password ? (
-            <div>{errors.password}</div>
-          ) : null}          
-          </label>
-
-          <label htmlFor="conf_password" className='block'>
-            <span className="text-neutral-800 dark:text-neutral-200">Confirm your password</span>
-          <Field name="conf_password" type="password" />
-          {errors.conf_password && touched.conf_password ? (
-            <div>{errors.conf_password}</div>
-          ) : null}    
-          </label>
+      {({ errors, touched, isSubmitting, handleSubmit }) => (
+        <Form
+          onSubmit={handleSubmit}
+        >
+          <Field
+            name="name"
+            type="text"
+            label="Name"
+            placeholder="Name"
+            component={Input}
+          />
+          <Field
+            name="document"
+            type="text"
+            label="Document"
+            placeholder="Document"
+            component={Input}
+          />
+          <Field
+            name="email"
+            type="email"
+            label="Email"
+            placeholder="Email"
+            component={Input}
+          />
+          <Field
+            name="password"
+            type="password"
+            label="Password"
+            placeholder="Password"
+            component={Input}
+          />
+          <Field
+            name="conf_password"
+            type="password"
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            component={Input}
+          />
           <ButtonPrimary
-            className="w-full max-w-md mx-auto space-y-6"
+            title="Register"
             type="submit"
-          >
-            Submit
-          </ButtonPrimary>
+            className="w-full mt-4"
+            disabled={isSubmitting}
+          />
         </Form>
       )}
     </Formik>
-  </div>
-);
+  )
+};
 
 export default FormComponent;
--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -4,28 +4,34 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   sizeClass?: string;
   fontClass?: string;
   rounded?: string;
+  field?: any;
 }
 
 // eslint-disable-next-line react/display-name
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   (
-    {
+    props,
+    ref
+  ) => {
+
+   const {
       className = "",
       sizeClass = "h-11 px-4 py-3",
       fontClass = "text-sm font-normal",
       rounded = "rounded-2xl",
       children,
       type = "text",
-      ...args
-    },
-    ref
-  ) => {
+      field,
+      ...rest
+    } = props;
+
     return (
       <input
         ref={ref}
         type={type}
         className={`block w-full border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900 disabled:bg-neutral-200 dark:disabled:bg-neutral-800 ${rounded} ${fontClass} ${sizeClass} ${className}`}
-        {...args}
+        {...field}
+        {...rest}
       />
     );
   }
