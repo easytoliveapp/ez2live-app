11ac0f0f67088374118bdcb1920497695ef1854a
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-08-08 16:40:41 -0300

    feat: typology and design corrections

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -7,14 +7,23 @@ const UserRegisterPage = () => {
 
   return (
     <div className={`nc-PageSignUp `} data-nc-id="PageSignUp">
-      <div className="container mb-24 lg:mb-32">
-        <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-black dark:text-neutral-100 justify-center">
-          Recuperar <br/> Senha
+      <div className="container mb-8 lg:mb-32">
+      <div className='mt-8 mb-16 flex items-center justify-between'>
+        <h2 className=" pl-6 flex items-center text-2xl leading-[115%] md:text-5xl md:leading-[115%] font-bold text-black dark:text-neutral-100 justify-center">
+          Recuperar <br/>
+          senha
         </h2>
+        <div>
+          <div className='relative rounded-full w-40 h-16 bg-gradient-to-r from-secondary-ez2live to-secondary-ez2livebg'>
+            <div className='absolute top-8 right-0 rounded-full w-16 h-16 bg-gradient-to-r from-secondary-ez2live to-secondary-ez2livebg'>
+            </div>
+          </div>
+        </div>          
+        </div>
         <div className="max-w-md mx-auto space-y-6 ">
           {/* FORM */}
           <FormComponent />
-          <span className=" block pt-10 text-center text-sm font-semibold text-black dark:text-neutral-300">
+          <span className=" block text-center text-sm font-semibold text-black dark:text-neutral-300">
             voltar para { }
             <Link className="text-primary-ez2live text-sm font-semibold" href="/login">
               login
--- src/app/resetPassword/[token]/page.tsx
+++ src/app/resetPassword/[token]/page.tsx
@@ -9,7 +9,7 @@ import { useRouter } from 'next/navigation'
 import Auth from "@/service/auth.service";
 
 
-const resetPassord = (token : any) => {
+const resetPassord = (token : string) => {
   const [loading, setLoading] = useState(false)
   const router = useRouter();
   const SignUpValidationSchema = Yup.object().shape({
@@ -33,9 +33,9 @@ const resetPassord = (token : any) => {
     conf_password: ""
   };
 
-  const handleFormSubmit = async (values : any) => {
+  const handleFormSubmit = async (values : IResetPassword) => {
     setLoading(true);
-    await Auth.resetPassword( values.password , token)
+    await Auth.resetPassword(  token, values.password)
     .then(()=>{
       //handleToast password resetada com successo
       router.push('/login')
@@ -48,10 +48,18 @@ const resetPassord = (token : any) => {
 
   return (
     <div className={`nc-PageSignUp `} data-nc-id="PageSignUp">
-      <div className="container mb-24 lg:mb-32">
-        <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-black dark:text-neutral-100 justify-center">
+      <div className="container mb-8 lg:mb-32">
+      <div className='mt-8 mb-16 flex items-center justify-between'>
+        <h2 className=" pl-6 flex items-center text-2xl leading-[115%] md:text-5xl md:leading-[115%] font-bold text-black dark:text-neutral-100 justify-center">
           Nova senha
         </h2>
+        <div>
+          <div className='relative rounded-full w-40 h-16 bg-gradient-to-r from-secondary-ez2live to-secondary-ez2livebg'>
+            <div className='absolute top-8 right-0 rounded-full w-16 h-16 bg-gradient-to-r from-secondary-ez2live to-secondary-ez2livebg'>
+            </div>
+          </div>
+        </div>          
+        </div>
         <div className="max-w-md mx-auto space-y-6 ">
           {/* FORM */}
           <Formik
