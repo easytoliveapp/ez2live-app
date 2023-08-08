87e010bb7887bc0f923cbc170c225c4c105c59af
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-08-08 10:40:33 -0300

    feat: register mult step

--- src/app/register/supplier/FormComponent.tsx
+++ src/app/register/supplier/FormComponent.tsx
@@ -9,10 +9,28 @@ import { useRouter } from 'next/navigation'
 import Auth from "@/service/auth.service";
 
 const FormComponent = () => {
-  const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0)
+  const [initialValues, setInitialValues] = useState(
+    {
+      name: "",
+      document: "",
+      email: "",
+      password: "",
+      supplierCategory: "",
+      isSupplier: true,
+      address: {
+      street: "",
+      number: "",
+      neighborhood: "",
+      city: "",
+      state: "",
+      zipcode: "",
+    },
+  })
 
-  const SignUpValidationSchema = Yup.object().shape({
+  const FirstStepValidationSchema = Yup.object().shape({
     name: Yup.string()
       .min(2, "Nome muito curto!")
       .max(50, "Nome muito longo!")
@@ -33,6 +51,9 @@ const FormComponent = () => {
       )
       .min(8, "Senha deve conter no mínimo 8 caracteres.")
       .max(36, "Senha não deve contar mais de 36 caracteres"),
+  });
+
+  const SecondStepValidationSchema = Yup.object().shape({
     address: Yup.object().shape({
       street: Yup.string().required("Campo requerido"),
       number: Yup.string().required("Campo requerido"),
@@ -43,48 +64,40 @@ const FormComponent = () => {
     }),
   });
 
-  const initialValues: IRegisterAccount = {
-    name: "",
-    document: "",
-    email: "",
-    password: "",
-    supplierCategory: "",
-    isSupplier: true,
-    address: {
-      street: "",
-      number: "",
-      neighborhood: "",
-      city: "",
-      state: "",
-      zipcode: "",
-    },
-  };
+  const handleNextStep = (newData : Partial<IRegisterAccount>) => {
+    setInitialValues( prev => ({...prev, ...newData}))
+    setCurrentStep(prev => prev + 1)
+  }
 
   const handleFormSubmit = async (values: IRegisterAccount) => {
     setLoading(true);
-      await Auth.register({
-        name: values.name,
-        email: values.email,
-        password: values.password,
-        document: values.document,
-        isSupplier: values.isSupplier,
-        address: values.address,
-      }).then(()=>{
+    await Auth.register({
+      name: values.name,
+      email: values.email,
+      password: values.password,
+      document: values.document,
+      isSupplier: values.isSupplier,
+      address: values.address,
+    }).then(()=>{
         //handleToast login success
         router.push('/')
-      })
-      .catch((error)=> {
+    }).catch((error)=> {
         console.log(error)
         //handleToast error in login
-      })
+    })
     setLoading(false);
   };
 
-  return (
-    <Formik
+  const StepOne : any = (props : any) => {
+    const handleSubmit = (values : Partial<IRegisterAccount>) => {
+      props.next(values)
+    }
+
+    return  (
+      <Formik
       initialValues={initialValues}
-      validationSchema={SignUpValidationSchema}
-      onSubmit={handleFormSubmit}
+      validationSchema={FirstStepValidationSchema}
+      onSubmit={handleSubmit}
     >
       {({ errors, touched, handleSubmit } : FormikProps<IRegisterAccount>) => (
         
@@ -143,86 +156,6 @@ const FormComponent = () => {
             />
           </FormItem>
           <FormItem
-            errorMessage={errors.address?.zipcode}
-            invalid={!!(errors.address?.zipcode && touched.address?.zipcode)}
-            label='CEP'
-          >
-            <Field
-              invalid = {!!(errors.address?.zipcode && touched.address?.zipcode)}
-              name="address.zipcode"
-              type="text"
-              label="CEP"
-              component={Input}
-            />
-          </FormItem>
-          
-          <FormItem
-            label='endereço'
-            errorMessage={errors.address?.street}
-            invalid={!!(errors.address?.street && touched.address?.street)}
-          >
-            <Field
-              invalid = {!!(errors.address?.street && touched.address?.street)}
-              name="address.street"
-              type="text"
-              label="street"
-              component={Input}
-            />
-          </FormItem>
-
-          <FormItem
-            label='numero'
-            errorMessage={errors.address?.number}
-            invalid={!!(errors.address?.number && touched.address?.number)}
-          >
-            <Field
-              invalid = {!!(errors.address?.number && touched.address?.number)}
-              name="address.number"
-              type="text"
-              label="number"
-              component={Input}
-            />
-          </FormItem>
-          <FormItem
-            label='bairro'
-            errorMessage={errors.address?.neighborhood}
-            invalid={!!(errors.address?.neighborhood && touched.address?.neighborhood)}
-          >
-            <Field
-              invalid = {!!(errors.address?.neighborhood && touched.address?.neighborhood)}
-              name="address.neighborhood"
-              type="text"
-              label="Bairro"
-              component={Input}
-            />
-          </FormItem>
-          <FormItem
-            label='cidade'
-            errorMessage={errors.address?.city}
-            invalid={!!(errors.address?.city && touched.address?.city)}
-          >
-            <Field
-              invalid = {!!(errors.address?.city && touched.address?.city)}
-              name="address.city"
-              type="text"
-              label="Cidade"
-              component={Input}
-            />
-          </FormItem>
-          <FormItem
-            label='estado'
-            errorMessage={errors.address?.state}
-            invalid={!!(errors.address?.state && touched.address?.state)}
-          >
-            <Field
-              invalid = {!!(errors.address?.state && touched.address?.state)}
-              name="address.state"
-              type="text"
-              label="Estado"
-              component={Input}
-            />
-          </FormItem>
-          <FormItem
             label='senha'
             errorMessage={errors.password}
             invalid={!!(errors.password && touched.password)}
@@ -243,7 +176,116 @@ const FormComponent = () => {
         </Form>
       )}
     </Formik>
-  );
+    )
+  }
+  const StepTwo: any = () => {
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={SecondStepValidationSchema}
+        onSubmit={handleFormSubmit}
+      >
+        {({ errors, touched, handleSubmit } : FormikProps<IRegisterAccount>) => (
+          
+          <Form onSubmit={handleSubmit} className="flex flex-col gap-3">
+            <FormItem
+              errorMessage={errors.address?.zipcode}
+              invalid={!!(errors.address?.zipcode && touched.address?.zipcode)}
+              label='CEP'
+            >
+              <Field
+                invalid = {!!(errors.address?.zipcode && touched.address?.zipcode)}
+                name="address.zipcode"
+                type="text"
+                label="CEP"
+                component={Input}
+              />
+            </FormItem>
+            
+            <FormItem
+              label='endereço'
+              errorMessage={errors.address?.street}
+              invalid={!!(errors.address?.street && touched.address?.street)}
+            >
+              <Field
+                invalid = {!!(errors.address?.street && touched.address?.street)}
+                name="address.street"
+                type="text"
+                label="street"
+                component={Input}
+              />
+            </FormItem>
+  
+            <FormItem
+              label='numero'
+              errorMessage={errors.address?.number}
+              invalid={!!(errors.address?.number && touched.address?.number)}
+            >
+              <Field
+                invalid = {!!(errors.address?.number && touched.address?.number)}
+                name="address.number"
+                type="text"
+                label="number"
+                component={Input}
+              />
+            </FormItem>
+            <FormItem
+              label='bairro'
+              errorMessage={errors.address?.neighborhood}
+              invalid={!!(errors.address?.neighborhood && touched.address?.neighborhood)}
+            >
+              <Field
+                invalid = {!!(errors.address?.neighborhood && touched.address?.neighborhood)}
+                name="address.neighborhood"
+                type="text"
+                label="Bairro"
+                component={Input}
+              />
+            </FormItem>
+            <FormItem
+              label='cidade'
+              errorMessage={errors.address?.city}
+              invalid={!!(errors.address?.city && touched.address?.city)}
+            >
+              <Field
+                invalid = {!!(errors.address?.city && touched.address?.city)}
+                name="address.city"
+                type="text"
+                label="Cidade"
+                component={Input}
+              />
+            </FormItem>
+            <FormItem
+              label='estado'
+              errorMessage={errors.address?.state}
+              invalid={!!(errors.address?.state && touched.address?.state)}
+            >
+              <Field
+                invalid = {!!(errors.address?.state && touched.address?.state)}
+                name="address.state"
+                type="text"
+                label="Estado"
+                component={Input}
+              />
+            </FormItem>
+            <ButtonPrimary
+              type="submit"
+              className="w-full mt-4"
+              disabled={loading}
+            >Avançar</ButtonPrimary>
+          </Form>
+        )}
+      </Formik>
+    );    
+  } 
+  const steps = [
+  <StepOne key={0} next={handleNextStep} data={initialValues}  /> , <StepTwo key={1} data={initialValues}  />]
+
+  return (
+    <div>
+      {steps[currentStep]}
+    </div>
+  )
 };
 
 export default FormComponent;
