572128a23dde387632ff201d463db83437e5ce77
renanpontez <renanpontez@gmail.com>
2023-08-15 18:32:11 -0300

    feat: improve secondary color and button

--- src/components/atoms/Button/ButtonSecondary.tsx
+++ src/components/atoms/Button/ButtonSecondary.tsx
@@ -4,12 +4,12 @@ import React from "react";
 export interface ButtonSecondaryProps extends ButtonProps {}
 
 const ButtonSecondary: React.FC<ButtonSecondaryProps> = ({
-  className = " border border-slate-300 dark:border-slate-700 ",
+  className = "",
   ...args
 }) => {
   return (
     <Button
-      className={`ttnc-ButtonSecondary bg-secondary-ez2live text-primary-ez2live text-bold dark:bg-slate-900 dark:text-slate-300 hover:bg-gray-100 dark:hover:bg-slate-800 ${className}`}
+      className={`ttnc-ButtonSecondary bg-secondary-ez2live text-primary-ez2live text-bold dark:bg-slate-900 dark:text-slate-300 hover:bg-secondary-ez2live_600 dark:hover:bg-slate-800 ${className}`}
       {...args}
     />
   );
