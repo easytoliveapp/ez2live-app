7dcfda07606ba96cdcc3439f62bf1146b28cf2ec
renanpontez <renanpontez@gmail.com>
2023-08-24 23:43:21 -0300

    feat: supplier improvements

--- src/app/supplier-dashboard/[id]/page.tsx
+++ src/app/supplier-dashboard/[id]/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import React, {useEffect , useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avaliation, SupplierCoupons } from '@/components/atoms/index';
 import Image from 'next/image';
 import BgSupplierExample from '@/images/easytolive/supplier/example-bg-supplier-dashboard.jpg';
 import SupplierService from '@/service/supplier.service';
-import {ISupplier} from '@/types/supplier'
+import { ISupplier } from '@/types/supplier'
 import SupplierLogo from '@/images/easytolive/logo/logotipo-fundoazulroxo.svg'
 import { useToastify } from '@/hooks/useToastify';
 import Link from 'next/link';
@@ -13,71 +13,73 @@ import Link from 'next/link';
 interface tokenProps {
   params: {
     id: string
-  }
-}
+  };
+};
 
-const SupplierDashBoard = ({ params }: tokenProps )=> {
-  const [supplier, setSupplier] = useState<ISupplier>()
+const SupplierDashBoard = ({ params }: tokenProps) => {
+  const [supplier, setSupplier] = useState<ISupplier>();
 
-  const getSupplierById = async(id:string) => {
-    const res: any = await SupplierService.getSupplierById(id)
-    return res
-  }
+  const getSupplierById = async (id: string) => {
+    const res: any = await SupplierService.getSupplierById(id);
+    return res;
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getSupplierById(params.id)
-    .then((res)=> {setSupplier(res?.data?.supplier)})
-    .catch((error)=>{
-      if(error?.response?.data?.code === 400) {
-        useToastify({ label: 'Oops! Parece que você acessou um endereço de estabelcimento errado', type: 'error'})
-      }
-    })
-  },[])
+      .then((res) => { setSupplier(res?.data?.supplier) })
+      .catch((error) => {
+        if (error?.response?.data?.code === 400) {
+          useToastify({ label: 'Oops! Parece que você acessou um endereço de estabelcimento errado', type: 'error' })
+        }
+      });
+  }, []);
 
   return (
-    <div className='relative'>
-      <Image
-      className='w-full max-w-md'
-      alt='bg-supplier-example'
-      src={BgSupplierExample}/>
-      <div
-      className='absolute top-40 py-8 px-5 rounded-t-3xl bg-primary-ez2livebg w-full'>
-        <div
-        className='flex items-center justify-between'>
-          <div className='flex gap-1'>
-          <p className='text-xs'>
-            {supplier?.supplierCategory.title} /  
-          </p>
-          <Link
-          href={`/supplier-dashboard/${params.id}`} className='text-xs underline'>
-          {supplier?.name}</Link>
-          </div>
-          <Avaliation note={'4.7'} />
-        </div>
+    <div className="md:w-[500px] w-full m-auto p-5">
+      <div className='relative'>
         <Image
-        className='w-12 my-4 h-auto rounded-full'
-        alt='Logo Image'
-        src={SupplierLogo}
+          className='w-full max-w-md'
+          alt='bg-supplier-example'
+          src={BgSupplierExample}
         />
+        <div className='absolute top-40 py-8 px-5 rounded-t-3xl bg-primary-ez2livebg w-full'>
+          <div className='flex items-center justify-between'>
+            <div className='flex gap-1'>
+              <Link href={`/`} className='text-xs underline'>
+                {supplier?.supplierCategory.title}
+              </Link>
+              <p className='text-xs'>
+               / {supplier?.name}
+              </p>
+            </div>
+            <Avaliation note={'4.7'} />
+          </div>
+          <Image
+            className='w-12 my-4 h-auto rounded-full'
+            alt='Logo Image'
+            src={SupplierLogo}
+          />
 
-        <h2 className=' text-xl font-semibold'>{supplier?.name}</h2>
-        <p className='pt-2 text-xs text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi vero velit quam repellendus facere ea recusandae, sapiente repudiandae perspiciatis temporibus et exercitationem illum nobis corrupti, sunt voluptates perferendis dicta fugiat.</p>
-        <div className='mt-4 flex flex-col gap-4'>
+          <h2 className=' text-xl font-semibold'>{supplier?.name}</h2>
+          <p className='pt-2 text-xs text-gray-400'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi vero velit quam repellendus facere ea recusandae, sapiente repudiandae perspiciatis temporibus
+            et exercitatione illum nobis corrupti, sunt voluptates perferendis dicta fugiat.</p>
+          <div className='mt-4 flex flex-col gap-4'>
 
-        { supplier?.coupons && (
-          supplier?.coupons.map((coupon, key)=> (
-            <SupplierCoupons
-            discount={coupon.discount}
-            expirateTime={5}
-            products='todos os produtos'
-            unintsAmount={20}
-            key={key}
-            />))
-          )
-        }
-        </div>  
+            {supplier?.coupons && (supplier?.coupons.map((coupon, key) => (
+              <SupplierCoupons
+                discount={coupon.discount}
+                expirateTime={5}
+                products='todos os produtos'
+                unintsAmount={20}
+                key={key}
+              />
+            )))
+            }
+          </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/components/atoms/SupplierCoupons/index.tsx
+++ src/components/atoms/SupplierCoupons/index.tsx
@@ -7,71 +7,76 @@ import CouponBlack from '@/images/easytolive/icons/couponblack.svg'
 import ShoppingCart from '@/images/easytolive/icons/shopping_cart.svg'
 import ClockCircle from '@/images/easytolive/icons/clock_circle.svg'
 import ArrowRight from '@/images/easytolive/icons/arrow-next-right.svg'
-
+import classNames from '@/utils/classNames';
 
 interface SupplierCouponsProps {
   discount: string;
   unintsAmount: number;
   expirateTime: number;
   products: string;
-
 }
 
-const SupplierCoupons : React.FC<SupplierCouponsProps> = ({
+const SupplierCoupons: React.FC<SupplierCouponsProps> = ({
   discount,
   unintsAmount,
   expirateTime,
   products
-})=> {
+}) => {
   return (
-    <div className='bg-white h-auto pl-4 rounded-full flex items-center gap-3'>
+    <div className='bg-white h-auto pl-4 rounded-full flex items-center gap-3 cursor-pointer'>
       <Image
-      className='h-10 w-auto'
-      alt='Coupons Image'
-      src={parseInt(discount) >= 30 ? CouponGreen: (parseInt(discount) >= 20)? CouponsYellow: CouponRed}/>
+        className='h-10 w-auto'
+        alt='Coupons Image'
+        src={parseInt(discount) >= 30 ? CouponGreen : (parseInt(discount) >= 20) ? CouponsYellow : CouponRed}
+      />
       <div
-    className={`rounded-full w-full py-2 px-2 gap-4 border-2
-    ${parseInt(discount) >= 30 ? 'border-secondary-ez2livegreen':
-    (parseInt(discount) >= 20)? 'border-secondary-ez2liveyellow' : 'border-secondary-ez2livered' }`}>
+        className={
+          classNames(
+            'rounded-full w-full py-2 px-2 gap-4 border-2 border-[#f2f2f2] hover:shadow-md active:border-[#d9d9d9]',
+          )
+        }
+      >
         <div className='Rounded-full flex items-center justify-evenly w-full'>
           <h2 className=' font-bold text-xl'>
-          {discount}%
+            {discount}%
           </h2>
           <span className='bg-gray-300 w-0.5 h-12'></span>
-        <div className='flex flex-col gap-1.5 text-xs' >
-          <p className='flex items-center'>
+          <div className='flex flex-col gap-1.5 text-xs' >
+            <p className='flex items-center'>
+              <Image
+                className='h-3.5 pr-2 w-auto'
+                alt='coupon-black'
+                src={CouponBlack}
+              />
+              faltam {unintsAmount} unidades</p>
+            <p className='flex items-center'>
+              <Image
+                className='h-3.5 pr-2 w-auto'
+                alt='coupon-black'
+                src={ClockCircle}
+              />
+              termina em {expirateTime} dias
+            </p>
+            <p className='flex items-center'>
+              <Image
+                className='h-3.5 pr-2 w-auto'
+                alt='coupon-black'
+                src={ShoppingCart}
+              />
+              {products}
+            </p>
+          </div>
+          <div>
             <Image
-            className='h-3.5 pr-2 w-auto'
-            alt='coupon-black'
-            src={CouponBlack}
+              className='h-6 w-auto'
+              alt='arrow right'
+              src={ArrowRight}
             />
-            faltam {unintsAmount} unidades</p>
-          <p className='flex items-center'>
-          <Image
-            className='h-3.5 pr-2 w-auto'
-            alt='coupon-black'
-            src={ClockCircle}
-            />
-            termina em {expirateTime} dias</p>
-          <p className='flex items-center'>
-          <Image
-            className='h-3.5 pr-2 w-auto'
-            alt='coupon-black'
-            src={ShoppingCart}
-            />
-            {products}</p>
+          </div>
         </div>
-       <div>
-        <Image
-        className='h-6 w-auto'
-        alt='arrow right'
-        src={ArrowRight}/>
-       </div>
       </div>
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
 export default SupplierCoupons;
