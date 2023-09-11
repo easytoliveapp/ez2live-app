dd57558a501862bab8c65ad0f30536888d53700f
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-09-10 22:39:25 -0300

    feat: merge conflicts

--- src/components/atoms/Navigation/NavMobile.tsx
+++ src/components/atoms/Navigation/NavMobile.tsx
@@ -8,8 +8,6 @@ import { NAVIGATION_DEMO_2 } from "@/data/navigation";
 import SocialsList from "@/components/atoms/SocialsList/SocialsList";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import EasyToLiveLogo from '@/images/easytolive/logo/logocompleta-semfundoazulroxo.svg' 
-import Image from 'next/image'
 
 export interface NavMobileProps {
   data?: NavItemType[];
@@ -114,57 +112,10 @@ const NavMobile: React.FC<NavMobileProps> = ({
     );
   };
 
-  const renderMagnifyingGlassIcon = () => {
-    return (
-      <svg
-        width={22}
-        height={22}
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M22 22L20 20"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    );
-  };
-
-  const renderSearchForm = () => {
-    return (
-      <form
-        action=""
-        method="POST"
-        className="flex-1 text-slate-900 "
-      >
-        <div className="bg-slate-50  flex items-center space-x-1 py-2 px-4 rounded-xl h-full">
-          {renderMagnifyingGlassIcon()}
-          <input
-            type="search"
-            placeholder="Type and press enter"
-            className="border-none bg-transparent focus:outline-none focus:ring-0 w-full text-sm "
-          />
-        </div>
-        <input type="submit" hidden value="" />
-      </form>
-    );
-  };
 
   return (
     <div className="overflow-y-auto w-full h-screen py-2 transition transform shadow-lg ring-1  bg-white  divide-y-2 divide-neutral-100 ">
       <div className="py-6 px-5">
-        <Logo />
         <div className="flex flex-col mt-5 text-slate-600  text-sm">
           <span>
             Discover the most outstanding articles on all topics of life. Write
@@ -173,9 +124,6 @@ const NavMobile: React.FC<NavMobileProps> = ({
 
           <div className="flex justify-between items-center mt-4">
             <SocialsList itemClass="w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-full text-xl" />
-            <span className="block">
-              <SwitchDarkMode className="bg-neutral-100 " />
-            </span>
           </div>
         </div>
         <span className="absolute right-2 top-2 p-1">
