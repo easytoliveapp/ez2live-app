7f5381a551b91548345061bf56240ab3ce9fc46e
Pedro Lima <pedro@deonibus.com>
2023-09-24 10:53:46 -0300

    fix: nextauth-setup -> add a prebuild script to change env var that starts with _ in netlify

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import isAuthenticated from "@/utils/isAuthenticated";
 
-// Limit the middleware to paths starting with `/api/`
-export const config = {
-  matcher: [
-    "/dashboard",
-    "/supplier-dashboard",
-    "/supplier-dashboard/:path*",
-    "/supplier-not-verified",
-  ],
-};
-
 export function middleware(request: NextRequest) {
   // Call our authentication function to check the request
   if (!isAuthenticated(request)) {
@@ -21,3 +11,13 @@ export function middleware(request: NextRequest) {
   // If the request is authenticated, continue to the API route handler
   return NextResponse.next();
 }
+
+// Limit the middleware to paths starting with `/api/`
+export const config = {
+  matcher: [
+    "/",
+    "/dashboard",
+    "/supplier-dashboard/:path*",
+    "/supplier-not-verified",
+  ],
+};
