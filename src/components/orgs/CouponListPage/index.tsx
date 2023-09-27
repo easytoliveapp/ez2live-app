856bfc6bf100b006f8f088decf80435b36a8a289
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-09-27 14:14:28 -0300

    feat: restore couponlistPage

--- src/components/orgs/CouponListPage/index.tsx
+++ src/components/orgs/CouponListPage/index.tsx
@@ -4,16 +4,23 @@ import Link from "next/link";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
-import { Avaliation, SupplierCoupons } from "@/components/atoms";
+import {
+  Avaliation,
+  SupplierCoupons,
+  ButtonSecondary,
+  ButtonThird,
+} from "@/components/atoms";
 import { ISupplier } from "@/types/supplier";
 import ArrowLeft from "@/images/easytolive/icons/arrow-next-right-white.svg";
 import supplierService from "@/service/supplier.service";
 import { showToastify } from "@/hooks/showToastify";
-
+import CouponPrimary from "@/images/easytolive/icons/couponPrimary.svg";
 import Arrow from "@/images/easytolive/icons/arrow-next-right-primary.svg";
 import Edit from "@/images/easytolive/icons/edit.svg";
 import LogoImage from "@/images/easytolive/logo/logotipo-fundoazulroxo.svg";
-import { useSession } from "next-auth/react";
+import { getItemByLocalStorage } from "@/utils/localStorageHelper";
+import { userLoginResponseProps } from "@/types/user";
+import { ModalEdit, CreateCoupon } from "@/components/mols";
 
 interface ICouponListPageProps {
   supplierId: string;
@@ -21,8 +28,8 @@ interface ICouponListPageProps {
 
 const CouponListPage: React.FC<ICouponListPageProps> = ({ supplierId }) => {
   const [supplier, setSupplier] = useState<ISupplier>();
-  const { data: session } = useSession();
-  const user = session?.user;
+  const [user, setUser] = useState<userLoginResponseProps>();
+  const [modalCreateCoupon, setModalCreateCoupon] = useState(false);
 
   const getSupplierById = async (id: string) => {
     const res: any = await supplierService.getSupplierById(id);
@@ -30,6 +37,9 @@ const CouponListPage: React.FC<ICouponListPageProps> = ({ supplierId }) => {
   };
 
   useEffect(() => {
+    const user = getItemByLocalStorage("user");
+    if (user) setUser(user);
+
     getSupplierById(supplierId)
       .then((res) => {
         setSupplier(res?.data?.supplier);
@@ -47,7 +57,22 @@ const CouponListPage: React.FC<ICouponListPageProps> = ({ supplierId }) => {
 
   return (
     <div className="relative md:w-[500px] h-full w-full mx-auto">
-      <div className="h-40 w-full bg-gradient-to-r from-primary-ez2lliveBlue to-primary-ez2live"></div>
+      <ModalEdit
+        closeOnBlur={false}
+        show={modalCreateCoupon}
+        onCloseModalEdit={() => setModalCreateCoupon(false)}
+      >
+        <div className="flex flex-col items-center w-full">
+          <CreateCoupon />
+          <ButtonThird
+            className="text-generic-alertRed"
+            onClick={() => setModalCreateCoupon(false)}
+          >
+            cancelar
+          </ButtonThird>
+        </div>
+      </ModalEdit>
+      <div className="h-40 w-full bg-gradient-to-r from-primary-lighter to-primary-main"></div>
       <Link
         className="absolute flex items-center justify-center rounded-full top-4 left-4 cursor-pointer h-8 w-8 bg-neutral-400 opacity-75 rotate-180"
         href={"/"}
@@ -59,7 +84,7 @@ const CouponListPage: React.FC<ICouponListPageProps> = ({ supplierId }) => {
         src={LogoImage}
         alt="Logo-restaurante"
       />
-      <div className="px-5 py-6 -mt-6 rounded-t-3xl bg-primary-ez2livebg w-full h-full">
+      <div className="px-5 py-6 -mt-6 rounded-t-3xl bg-generic-background w-full h-full">
         <div className="flex items-center justify-between">
           <div className="flex gap-1">
             <Link href={`/`} className="text-xs underline">
@@ -67,14 +92,29 @@ const CouponListPage: React.FC<ICouponListPageProps> = ({ supplierId }) => {
             </Link>
             <p className="text-xs">/ {supplier?.name}</p>
           </div>
-          <Avaliation note={"4.7"} />
+          <div className="flex flex-col">
+            <Avaliation note={"4.7"} />
+          </div>
         </div>
-        <Image
-          className="w-12 my-4 h-auto rounded-full"
-          alt="Logo Image"
-          src={LogoImage}
-        />
-
+        <div className="flex justify-between items-center">
+          <Image
+            className="w-12 my-4 h-auto rounded-full"
+            alt="Logo Image"
+            src={LogoImage}
+          />
+          <div>
+            {supplier?.id == user?.id && (
+              <ButtonSecondary onClick={() => setModalCreateCoupon(true)}>
+                <Image
+                  src={CouponPrimary}
+                  className="w-6 mr-3 h-auto"
+                  alt="coupon-image"
+                />
+                Novo Cupom
+              </ButtonSecondary>
+            )}
+          </div>
+        </div>
         <h2 className=" text-xl font-semibold">{supplier?.name}</h2>
         <p className="pt-2 text-xs text-gray-400">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
