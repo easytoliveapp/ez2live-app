2f412ad2f18e01fdf7de177bb061a9edf88b0889
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-10-01 21:04:38 -0300

    feat: build problem2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ function PageHome() {
   useEffect(() => {
     if (session?.user?.isSupplier) {
       if (!session?.user?.isVerified) {
-        router.push("/");
+        router.push("/supplier-not-verified");
       } else {
         setControlModalSupplierUploadRegister(true);
       }
