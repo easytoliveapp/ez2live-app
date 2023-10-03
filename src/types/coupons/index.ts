09c9275c002633729b6d530118eea91618f64eb3
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-10-03 00:21:14 -0300

    feat: first

--- src/types/coupons/index.ts
+++ src/types/coupons/index.ts
@@ -19,3 +19,15 @@ export interface ICoupon {
   supplier: string;
   id: string;
 }
+
+export interface ICouponsByUser {
+  id: string;
+  status: string;
+  usageDate: string;
+  title: string;
+  discount: string;
+  maxTotal: number;
+  maxPerUser: number;
+  expirationGenerationDate: string;
+  expirationUseDate: string;
+}
