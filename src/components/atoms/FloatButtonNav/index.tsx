d83181200deee2ad4e81f54e5ad595c9a20419c0
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-10-04 11:20:04 -0300

    feat: layout improvments

--- src/components/atoms/FloatButtonNav/index.tsx
+++ src/components/atoms/FloatButtonNav/index.tsx
@@ -3,17 +3,20 @@ import React from "react";
 import Image from "next/image";
 import NextLink, { LinkProps } from "next/link";
 import { UrlObject } from "url";
+import { motion } from "framer-motion";
 
 interface IFloatButtonNavProps {
   icon: StaticImageData;
   backGround: "main" | "secondary";
   href: UrlObject | string;
+  couponActive?: boolean;
 }
 
 const FloatButtonNav: React.FC<IFloatButtonNavProps & LinkProps<any>> = ({
   href,
   backGround,
   icon,
+  couponActive = false,
 }) => {
   return (
     <NextLink
@@ -21,10 +24,23 @@ const FloatButtonNav: React.FC<IFloatButtonNavProps & LinkProps<any>> = ({
       className={`${
         backGround == "main"
           ? "from-primary-main to-white"
-          : "from-secondary-dark to-secondary-lighter"
-      } bg-gradient-to-t rounded-l-full w-24 fixed bottom-28 right-0 z-50 min-h-8 p-3`}
+          : "from-secondary-main to-secondary-lighter"
+      } flex items-center bg-gradient-to-r rounded-l-full pl-4 py-4 pr-7 fixed bottom-28 right-0 z-50 min-h-8 p-3`}
     >
-      <Image className="w-8 h-auto" alt="icon-image" src={icon}></Image>
+      <Image className="w-9 h-auto" alt="icon-image" src={icon}></Image>
+      {couponActive && (
+        <div className="w-9 h-9 rouded-full flex items-center relative">
+          <motion.div
+            className="flex items-center justify-center ml-2 w-5 h-5 rounded-full bg-generic-alertGreenLigther"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 10 }}
+            transition={{ duration: 1.5, ease: "linear", repeat: Infinity }}
+          >
+            <div className="flex items-center justify-center w-3 h-3 rounded-full bg-generic-alertGreenLight"></div>
+          </motion.div>
+          <div className="absolute right-[15px] w-1.5 h-1.5 rounded-full bg-generic-alertGreen"></div>
+        </div>
+      )}
     </NextLink>
   );
 };
