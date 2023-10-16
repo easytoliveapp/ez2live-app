6719883ce62e58fed01b80ba5d6a6d369faf7d4c
Pedro Lima <pedro@deonibus.com>
2023-10-16 13:57:18 -0300

    fix: redirect on login

--- src/utils/isAuthenticated.ts
+++ src/utils/isAuthenticated.ts
@@ -2,17 +2,6 @@ import { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export default async function isAuthenticated(request: NextRequest) {
-  const tokenKey =
-    process.env.NODE_ENV === "production"
-      ? "__Secure-next-auth.session-token"
-      : "next-auth.session-token";
-
-  const hasActiveSession = request.cookies.get(tokenKey);
-
-  if (!hasActiveSession) {
-    return false;
-  }
-
   const token = await getToken({ req: request });
 
   return token;
