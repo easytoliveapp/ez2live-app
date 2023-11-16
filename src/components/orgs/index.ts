397f454aae5cb25211169dd313ee29b9e8e5bf50
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-11-16 16:12:27 -0300

    feat: start cleaning components not used

--- src/components/orgs/index.ts
+++ src/components/orgs/index.ts
@@ -1,4 +1,2 @@
-export { default as Header } from "./Header";
-export { default as HeaderLogged } from "./HeaderLogged";
 export { default as CouponList } from "./Coupon/List";
 export { default as CouponContainer } from "./Coupon/Container";
