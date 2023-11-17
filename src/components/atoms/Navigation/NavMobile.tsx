8cfe4d87e7073b0d44f02629cf903fbfd9311c2d
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-11-17 15:03:04 -0300

    feat: removing unused components

--- src/components/atoms/Navigation/NavMobile.tsx
+++ src/components/atoms/Navigation/NavMobile.tsx
@@ -2,12 +2,8 @@
 
 import React from "react";
 import ButtonClose from "@/components/atoms/ButtonClose/ButtonClose";
-import { Disclosure } from "@/app/headlessui";
 import { NavItemType } from "./NavigationItem";
-import { NAVIGATION_DEMO_2 } from "@/data/navigation";
 import SocialsList from "@/components/atoms/SocialsList/SocialsList";
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
-import Link from "next/link";
 import Image from "next/image";
 import extendedLogoImage from "@/images/easytolive/logo/logocompleta-semfundoazulroxo.svg";
 
@@ -16,97 +12,7 @@ export interface NavMobileProps {
   onClickClose?: () => void;
 }
 
-const NavMobile: React.FC<NavMobileProps> = ({
-  data = NAVIGATION_DEMO_2,
-  onClickClose,
-}) => {
-  const _renderMenuChild = (
-    item: NavItemType,
-    itemClass = " pl-3 text-neutral-900  font-medium ",
-  ) => {
-    return (
-      <ul className="nav-mobile-sub-menu pl-6 pb-1 text-base">
-        {item.children?.map((i, index) => (
-          <Disclosure key={index} as="li">
-            <Link
-              href={{
-                pathname: i.href || undefined,
-              }}
-              className={`flex text-sm rounded-lg hover:bg-neutral-100  mt-0.5 pr-4 ${itemClass}`}
-            >
-              <span
-                className={`py-2.5 ${!i.children ? "block w-full" : ""}`}
-                onClick={onClickClose}
-              >
-                {i.name}
-              </span>
-              {i.children && (
-                <span
-                  className="flex items-center flex-grow"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <Disclosure.Button
-                    as="span"
-                    className="flex justify-end flex-grow"
-                  >
-                    <ChevronDownIcon
-                      className="ml-2 h-4 w-4 text-slate-500"
-                      aria-hidden="true"
-                    />
-                  </Disclosure.Button>
-                </span>
-              )}
-            </Link>
-            {i.children && (
-              <Disclosure.Panel>
-                {_renderMenuChild(i, "pl-3 text-slate-600  ")}
-              </Disclosure.Panel>
-            )}
-          </Disclosure>
-        ))}
-      </ul>
-    );
-  };
-
-  const _renderItem = (item: NavItemType, index: number) => {
-    return (
-      <Disclosure key={index} as="li" className="text-slate-900 ">
-        <Link
-          className="flex w-full items-center py-2.5 px-4 font-medium uppercase tracking-wide text-sm hover:bg-slate-100  rounded-lg"
-          href={{
-            pathname: item.href || undefined,
-          }}
-        >
-          <span
-            className={!item.children ? "block w-full" : ""}
-            onClick={onClickClose}
-          >
-            {item.name}
-          </span>
-          {item.children && (
-            <span
-              className="block flex-grow"
-              onClick={(e) => e.preventDefault()}
-            >
-              <Disclosure.Button
-                as="span"
-                className="flex justify-end flex-grow"
-              >
-                <ChevronDownIcon
-                  className="ml-2 h-4 w-4 text-neutral-500"
-                  aria-hidden="true"
-                />
-              </Disclosure.Button>
-            </span>
-          )}
-        </Link>
-        {item.children && (
-          <Disclosure.Panel>{_renderMenuChild(item)}</Disclosure.Panel>
-        )}
-      </Disclosure>
-    );
-  };
-
+const NavMobile: React.FC<NavMobileProps> = ({ onClickClose }) => {
   return (
     <div className="overflow-y-auto w-full h-screen py-2 transition transform shadow-lg ring-1  bg-white  divide-y-2 divide-neutral-100 ">
       <div className="py-6 px-5">
@@ -122,9 +28,7 @@ const NavMobile: React.FC<NavMobileProps> = ({
           <ButtonClose onClick={onClickClose} />
         </span>
       </div>
-      <ul className="flex flex-col py-6 px-2 space-y-1">
-        {data.map(_renderItem)}
-      </ul>
+      <ul className="flex flex-col py-6 px-2 space-y-1"></ul>
     </div>
   );
 };
