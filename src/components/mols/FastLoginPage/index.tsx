35023938ce0c92dcea869546d3e141efeb2879aa
Felipe Macedo Gomes <femacedogomes@gmail.com>
2023-11-28 10:37:44 -0300

    fix: build problem

--- src/components/mols/FastLoginPage/index.tsx
+++ src/components/mols/FastLoginPage/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ButtonPrimary, ButtonThird, SocialLoginComponent } from "@/components";
+import { ButtonPrimary, SocialLoginComponent } from "@/components";
 import React, { useState } from "react";
 import PreLoginImage from "@/images/easytolive/home/fast-login-background.jpeg";
 import Image from "next/image";
@@ -10,7 +10,6 @@ import { motion } from "framer-motion";
 
 const FastLoginPage = () => {
   const [loadingLogin, setLoadingLogin] = useState(false);
-  const [loadingRegister, setLoadingRegister] = useState(false);
   const router = useRouter();
   const params = useSearchParams();
   const callbackUrl = params.get("callbackUrl");
@@ -24,17 +23,6 @@ const FastLoginPage = () => {
     );
     setTimeout(() => {
       setLoadingLogin(false);
-    }, 1500);
-  };
-  const handleRedirectToRegister = async () => {
-    setLoadingRegister(true);
-    router.push(
-      callbackUrl
-        ? `/conta/cadastrar/usuario?callbackUrl=${callbackUrl}`
-        : "/conta/cadastrar/usuario",
-    );
-    setTimeout(() => {
-      setLoadingRegister(false);
     }, 1500);
   };
 
