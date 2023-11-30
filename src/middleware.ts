9878e6a740413f1422709ca28973d84ac929be6f
Pedro Lima <pedro@deonibus.com>
2023-11-29 23:42:38 -0300

    feat: add route security based on role

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 import isAuthenticated from "@/utils/isAuthenticated";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // Call our authentication function to check the request
+
   if (!isAuthenticated(request)) {
-    console.log("debug preview.");
     // Respond with JSON indicating an error message
     return NextResponse.redirect(new URL("/conta/entrar", request.url));
   }
 
-  // If the request is authenticated, continue to the API route handler
+  const tokenInfo = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!tokenInfo) {
+    return NextResponse.redirect(new URL("/conta/entrar", request.url));
+  }
+
+  // If the user is not an admin or supplier and is trying to access the admin or dashboard, redirect to the home page
+  if (
+    tokenInfo.user.role === "user" &&
+    (request.nextUrl.pathname.includes("/dashboard") ||
+      request.nextUrl.pathname.includes("/admin"))
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // If the request is authenticated and authorized, continue to the API route handler
   return NextResponse.next();
 }
 
 // Limit the middleware to paths starting with `/api/`
 export const config = {
   matcher: [
+    "/admin/:path*",
     "/meus-cupons",
-    "/dashboard",
+    "/dashboard/:path*",
     "/dashboard/parceiro/:path*",
     "/parceiro-nao-encontrado",
     "/minha-conta",
