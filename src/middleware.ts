d4c6f193b3d93fc492eb43e8d3ce9752f13a5d16
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2023-12-10 15:44:20 -0300

    feat: new middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import isAuthenticated from "@/utils/isAuthenticated";
-import {
-  ADMIN_USERS_ROUTES,
-  REFUSE_COMMUM_USERS,
-  REFUSE_NON_LOGGED_USERS,
-  SUPPLIER_USERS_ROUTES,
-} from "./constants/PrivateRoutes";
+import { REFUSE_NON_LOGGED_USERS } from "./constants/PrivateRoutes";
+import { ROUTES_CONFIG } from "./routers";
 
 export async function middleware(request: NextRequest) {
   // Call our authentication function to check the request
@@ -40,49 +36,17 @@ export async function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL("/conta/entrar", request.url));
     }
   }
-
-  // If the user is not an admin or supplier and is trying to access the admin or dashboard, redirect to the home page
+  const requestedRouteInfo = ROUTES_CONFIG.filter(
+    (config) => config.path === request.nextUrl.pathname,
+  ).shift();
+  console.log(requestedRouteInfo);
   if (
-    tokenInfo?.user.role === "user" &&
-    REFUSE_COMMUM_USERS.some((route) =>
-      request.nextUrl.pathname.startsWith(route),
-    )
+    !requestedRouteInfo?.isPublic &&
+    !requestedRouteInfo?.roles.some((role) => role === tokenInfo?.user.role)
   ) {
     return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  // If the user is supplier account and is trying to access the admin or my-coupons, redirect to the dashboard page.
-  if (
-    tokenInfo?.user.role === "supplier" &&
-    !SUPPLIER_USERS_ROUTES.some((route) =>
-      request.nextUrl.pathname.startsWith(route),
-    )
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
-  // If the user is an admin will only be able to access routes starting with /admin
-  if (
-    tokenInfo?.user.role === "admin" &&
-    !ADMIN_USERS_ROUTES.some((route) =>
-      request.nextUrl.pathname.startsWith(route),
-    )
-  ) {
-    return NextResponse.redirect(new URL("/admin/parceiros", request.url));
   }
 
   // If the request is authenticated and authorized, continue to the API route handler
   return NextResponse.next();
 }
-
-// Limit the middleware to paths starting with `/api/`
-export const config = {
-  matcher: [
-    "/admin/:path*",
-    "/meus-cupons",
-    "/dashboard/:path*",
-    "/dashboard/parceiro/:path*",
-    "/parceiro-nao-encontrado",
-    "/minha-conta",
-    "/",
-  ],
-};
