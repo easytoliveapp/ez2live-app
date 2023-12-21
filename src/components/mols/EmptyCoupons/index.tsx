41a757cda04daa9f925e47d6a68ba19f76748807
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2023-12-21 12:20:52 -0300

    feat: improv code and change margin

--- src/components/mols/EmptyCoupons/index.tsx
+++ src/components/mols/EmptyCoupons/index.tsx
@@ -38,14 +38,19 @@ const EmptyCoupons: React.FC<IEmptyCoupons> = ({
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <Image
-        className="w-24 m-8 bg-white p-4 h-auto rounded-full "
+        className="w-24 m-2 bg-white p-4 h-auto rounded-full "
         src={icon}
         alt="Imagem Cupom"
       />
       <h3 className="text-center text-lg font-semibold mb-10">{title}</h3>
-      {label && href && <RedirectLink href={href} label={label} />}
-      {label && !href && (
-        <p className=" font-semibold text-primary-main">{label}</p>
+      {label && (
+        <>
+          {href ? (
+            <RedirectLink href={href} label={label} />
+          ) : (
+            <p className=" font-semibold text-primary-main">{label}</p>
+          )}
+        </>
       )}
     </div>
   );
