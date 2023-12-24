9247edc8a6901999104e23878f49028243a1c294
Pedro Lima <pedrohblima03@gmail.com>
2023-12-24 18:04:51 -0300

    fix: test remove url redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,14 +17,12 @@ export async function middleware(request: NextRequest) {
         : "next-auth.session-token",
   });
 
-  console.log("tokenInfo", tokenInfo);
-
-  if (
+/*   if (
     !tokenInfo &&
     !AUTH_ROUTE_PATHS.some((path) => path === request.nextUrl.pathname)
   ) {
     return NextResponse.redirect(new URL("/conta/entrar", request.url));
-  }
+  } */
 
   const privateRequestRoute = PRIVATE_ROUTES_CONFIG.filter((routes) => {
     return routes.path === request.nextUrl.pathname;
