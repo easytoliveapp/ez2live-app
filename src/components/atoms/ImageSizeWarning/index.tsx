37f2c7fcc2247a1aafc49ff4a2de3c332855b350
Pedro Lima <pedrohblima03@gmail.com>
2023-12-31 16:32:37 -0300

    build: Eslint build errors fix

--- src/components/atoms/ImageSizeWarning/index.tsx
+++ src/components/atoms/ImageSizeWarning/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import WidthIcon from "@/images/easytolive/icons/largura-48.svg";
 import HeigthIcon from "@/images/easytolive/icons/altura-48.svg";
 
 export interface IImageSizeWarning {
-  recommendedWidth: number,
-  recommendedHeight: number,
+  recommendedWidth: number;
+  recommendedHeight: number;
 }
 
 function ImageSizeWarning({
   recommendedWidth,
-  recommendedHeight
+  recommendedHeight,
 }: IImageSizeWarning) {
   return (
     <div className="p-4 bg-[#6722ff0d] rounded-lg">
@@ -21,26 +21,16 @@ function ImageSizeWarning({
       </div>
       <div className="flex justify-center items-center">
         <div className="flex gap-2 items-center w-1/2">
-          <Image
-            src={WidthIcon}
-            width={24}
-            height={24}
-            alt={"width icon"}
-          />
+          <Image src={WidthIcon} width={24} height={24} alt={"width icon"} />
           <p className="text-primary-main">{recommendedWidth} px</p>
         </div>
         <div className="flex gap-2 items-center w-1/2">
-          <Image
-            src={HeigthIcon}
-            width={24}
-            height={24}
-            alt={"heigth icon"}
-          />
+          <Image src={HeigthIcon} width={24} height={24} alt={"heigth icon"} />
           <p className="text-primary-main">{recommendedHeight} px</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ImageSizeWarning
+export default ImageSizeWarning;
