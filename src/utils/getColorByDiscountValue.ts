1757fefcbc3faef1c877b3527f8fe6e1e4584bbe
renanpontez <renanpontez@gmail.com>
2024-01-03 12:01:45 -0300

    feat: improve colors

--- src/utils/getColorByDiscountValue.ts
+++ src/utils/getColorByDiscountValue.ts
@@ -1,14 +1,15 @@
-export function getColorByDiscountValue(discount: string) {
-  const discountValue = Number(discount);
+export function getColorByDiscountValue(discount: string): string {
+  const discountValue: number = Number(discount);
 
-  const colorOptions = [
-    { value: 76, color: "bg-generic-gold" },
-    { value: 26, color: "bg-generic-lightGreen" },
+  const colorOptions: { value: number; color: string }[] = [
+    { value: 76, color: "bg-generic-electricGold" },
+    { value: 51, color: "bg-generic-gold" },
+    { value: 26, color: "bg-generic-limeGreen" },
   ];
+
   const selectedColor = colorOptions.find(
     ({ value }) => discountValue >= value,
-  ) || {
-    color: "bg-generic-lightBlue",
-  };
+  ) || { color: "bg-generic-skyBlue" };
+
   return selectedColor.color;
 }
