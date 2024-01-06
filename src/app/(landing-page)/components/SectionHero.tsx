0ddfbb83fb221f5c6f0e4977f9d666d3db1bf0a5
renanpontez <renanpontez@gmail.com>
2024-01-06 12:06:57 -0300

    feat: improve lp

--- src/app/(landing-page)/components/SectionHero.tsx
+++ src/app/(landing-page)/components/SectionHero.tsx
@@ -16,24 +16,28 @@ const SectionHero3: FC<SectionHero3Props> = ({ className = "" }) => {
     >
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-[80%] mx-auto my-5">
         {/* 1st column */}
-        <div className="flex flex-col items-start space-y-5">
+        <div className="flex flex-col md:items-start items-center space-y-5 md:pt-24">
           <span className="sm:text-lg md:text-xl font-semibold text-neutral-900">
-            Conheça a EasyToLive 🔥
+            Vem conhecer a{" "}
+            <span className="text-primary-main">EasyToLive!</span>
           </span>
-          <h2 className="font-bold text-black text-3xl sm:text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl !leading-[115%]">
-            Os melhores descontos nas melhores lojas
+          <h2 className="font-bold text-black text-3xl text-center md:text-left sm:text-4xl md:text-4xl xl:text-5xl 2xl:text-6xl !leading-[115%]">
+            Os melhores descontos, os melhores parceiros, pra você!
           </h2>
-          <div className="pt-4 gap-3">
-            <ButtonPrimary href="/app/conta/acessar">
-              Criar minha conta
-            </ButtonPrimary>
-
-            <ButtonThird
-              href="#EasyToLive"
-              className="text-sm sm:text-base lg:text-lg font-medium text-primary-lighter hover:text-primary-main"
-            >
-              Conhecer mais
-            </ButtonThird>
+          <div className="pt-4 gap-3 grid">
+            <div>
+              <ButtonPrimary href="/app/conta/acessar" className="text-slate-2">
+                Criar minha conta agora
+              </ButtonPrimary>
+            </div>
+            <div>
+              <ButtonThird
+                href="/app"
+                className="text-sm sm:text-base font-medium text-primary-lighter hover:text-primary-main text-slate-0"
+              >
+                Quero ver os descontos
+              </ButtonThird>
+            </div>
           </div>
         </div>
 
