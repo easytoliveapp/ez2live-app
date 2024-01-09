5ade513d9833f829eb788943a15641664aba1bd9
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-01-09 11:13:08 -0300

    feat: test

--- src/app/app/(authentication)/conta/acessar/layout.tsx
+++ src/app/app/(authentication)/conta/acessar/layout.tsx
@@ -4,5 +4,5 @@ export default function AuthenticationLayout({
   children: React.ReactNode;
   params: any;
 }) {
-  return <div className="relative h-screen">{children}</div>;
+  return <div className="relative h-screen max-h-screen">{children}</div>;
 }
