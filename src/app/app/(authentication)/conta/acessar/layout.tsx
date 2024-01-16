97e3c149719b27b22f7ca873a409e33ff1c3decd
Pedro Lima <pedrohblima03@gmail.com>
2024-01-16 18:54:09 -0300

    fix: remove unused type for layout

--- src/app/app/(authentication)/conta/acessar/layout.tsx
+++ src/app/app/(authentication)/conta/acessar/layout.tsx
@@ -2,7 +2,6 @@ export default function AuthenticationLayout({
   children,
 }: {
   children: React.ReactNode;
-  params: any;
 }) {
   return <div className="relative h-screen max-h-screen">{children}</div>;
 }
