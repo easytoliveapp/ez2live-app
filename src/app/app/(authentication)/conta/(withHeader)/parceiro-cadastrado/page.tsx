55de38ab56e11f7caa822896a6789f331b61f909
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-01-22 16:36:39 -0300

    feat: redirect when supplier isverified and try to access this route

--- src/app/app/(authentication)/conta/(withHeader)/parceiro-cadastrado/page.tsx
+++ src/app/app/(authentication)/conta/(withHeader)/parceiro-cadastrado/page.tsx
@@ -1,20 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import Ez2LiveLogo from "@/images/easytolive/logo/logocompleta-semfundoazulroxo.svg";
 import { ButtonPrimary } from "@/components";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const SupplierRegistered = () => {
   const router = useRouter();
+
+  const { data: session } = useSession();
+  useEffect(() => {
+    if (session?.user.supplierInfo?.isVerified === true) {
+      router.push("/app/dashboard");
+    }
+  }, []);
+
   return (
     <div className="flex h-[calc(100vh-120px)] flex-col items-center justify-between">
       <span></span>
       <Image alt="Ez-2-live-logo" src={Ez2LiveLogo} className="w-auto h-28" />
       <div className="flex flex-col gap-1 w-full justify-center items-center">
         <h2 className="pt-2 text-lg font-semibold">
-          Cadastro criando com succeso!
+          Cadastro criado com succeso!
         </h2>
         <p className="p-4 max-w-xl">
           sua conta como parceiro EasyToLive foi enviada para análise do nosso
