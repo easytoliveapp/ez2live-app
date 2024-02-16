4b2cbdc21921e603b4083aab718a7e9adc893139
Pedro Lima <pedrohblima03@gmail.com>
2024-02-15 23:32:53 -0300

    fix: resolve comments

--- src/app/app/(authentication)/conta/(withHeader)/(public)/cadastrar/parceiro/FormComponent.tsx
+++ src/app/app/(authentication)/conta/(withHeader)/(public)/cadastrar/parceiro/FormComponent.tsx
@@ -11,6 +11,7 @@ import Supplier from "@/service/supplier.service";
 import { ICategoryProps } from "@/types/supplier";
 import { showToastify } from "@/hooks/showToastify";
 import { MASKS } from "@/constants/masks";
+import { validateCnpj } from "@/utils/validateCNPJ";
 
 export interface IStepOneProps {
   next: (e: any) => void;
@@ -46,42 +47,6 @@ const FormComponent = () => {
     const res: any = await Supplier.getSupplierCategories();
     return res;
   };
-  function validateCnpj(cnpj: any) {
-    cnpj = cnpj.replace(/[^\d]+/g, "");
-
-    if (cnpj === "") return false;
-
-    if (cnpj.length !== 14) return false;
-
-    // Validate repetitive numbers
-    if (/^(\d)\1+$/.test(cnpj)) return false;
-
-    let length = cnpj.length - 2;
-    let numbers = cnpj.substring(0, length);
-    const digits = cnpj.substring(length);
-    let sum = 0;
-    let pos = length - 7;
-
-    for (let i = length; i >= 1; i--) {
-      sum += numbers.charAt(length - i) * pos--;
-      if (pos < 2) pos = 9;
-    }
-    let result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-    if (result != digits.charAt(0)) return false;
-
-    length = length + 1;
-    numbers = cnpj.substring(0, length);
-    sum = 0;
-    pos = length - 7;
-    for (let i = length; i >= 1; i--) {
-      sum += numbers.charAt(length - i) * pos--;
-      if (pos < 2) pos = 9;
-    }
-    result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-    if (result != digits.charAt(1)) return false;
-
-    return true;
-  }
 
   useEffect(() => {
     getSupplierCatogires()
@@ -110,7 +75,7 @@ const FormComponent = () => {
       .required("Campo nome é requerido"),
     document: Yup.string()
       .required("Campo requerido")
-      .test("cnpj", "Invalid CNPJ", (value) => validateCnpj(value)),
+      .test("cnpj", "CNPJ Inválido", (value) => validateCnpj(value)),
     supplierInfo: Yup.object().shape({
       supplierCategory: Yup.string()
         .nonNullable()
