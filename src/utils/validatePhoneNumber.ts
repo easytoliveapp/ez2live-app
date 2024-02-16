806b785a3b4e79272a72196aeeefd29f770e7272
renan m <renanpontez@gmail.com>
2024-02-16 13:01:16 -0300

    feat: type and translate code

--- src/utils/validatePhoneNumber.ts
+++ src/utils/validatePhoneNumber.ts
@@ -1,48 +1,53 @@
-export function validatePhoneNumber(telefone) {
-  //retira todos os caracteres menos os numeros
-  telefone = telefone.replace(/\D/g, "");
+export function validatePhoneNumber(phoneNumber: string): boolean {
+  // Remove all non-numeric characters
+  phoneNumber = phoneNumber.replace(/\D/g, "");
 
-  //verifica se tem a qtde de numero correto
-  if (!(telefone.length >= 10 && telefone.length <= 11)) return false;
+  // Check if the number of digits is correct
+  if (!(phoneNumber.length >= 10 && phoneNumber.length <= 11)) return false;
 
-  //Se tiver 11 caracteres, verificar se começa com 9 o celular
-  if (telefone.length == 11 && parseInt(telefone.substring(2, 3)) != 9)
+  // If there are 11 characters, check if it starts with 9 (cell phone)
+  if (phoneNumber.length === 11 && parseInt(phoneNumber.substring(2, 3)) !== 9)
     return false;
 
-  //verifica se não é nenhum numero digitado errado (propositalmente)
+  // Check if it is not any intentionally repeated number
   for (let n = 0; n < 10; n++) {
-    //um for de 0 a 9.
-    //estou utilizando o metodo Array(q+1).join(n) onde "q" é a quantidade e n é o
-    //caractere a ser repetido
-    if (telefone == new Array(11).join(n) || telefone == new Array(12).join(n))
+    // A loop from 0 to 9.
+    // Using the Array(q+1).join(n) method where "q" is the quantity and n is the
+    // character to be repeated
+    if (
+      phoneNumber === new Array(11).join(n) ||
+      phoneNumber === new Array(12).join(n)
+    )
       return false;
   }
-  //DDDs validos
-  const codigosDDD = [
+
+  // Valid area codes (DDDs)
+  const validAreaCodes: number[] = [
     11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 24, 27, 28, 31, 32, 33, 34, 35,
     37, 38, 41, 42, 43, 44, 45, 46, 47, 48, 49, 51, 53, 54, 55, 61, 62, 64, 63,
     65, 66, 67, 68, 69, 71, 73, 74, 75, 77, 79, 81, 82, 83, 84, 85, 86, 87, 88,
     89, 91, 92, 93, 94, 95, 96, 97, 98, 99,
   ];
-  //verifica se o DDD é valido (sim, da pra verificar rsrsrs)
-  if (codigosDDD.indexOf(parseInt(telefone.substring(0, 2))) == -1)
+
+  // Check if the area code (DDD) is valid
+  if (validAreaCodes.indexOf(parseInt(phoneNumber.substring(0, 2))) === -1)
     return false;
 
-  //  E por ultimo verificar se o numero é realmente válido. Até 2016 um celular pode
-  //ter 8 caracteres, após isso somente numeros de telefone e radios (ex. Nextel)
-  //vão poder ter numeros de 8 digitos (fora o DDD), então esta função ficará inativa
-  //até o fim de 2016, e se a ANATEL realmente cumprir o combinado, os numeros serão
-  //validados corretamente após esse período.
-  //NÃO ADICIONEI A VALIDAÇÂO DE QUAIS ESTADOS TEM NONO DIGITO, PQ DEPOIS DE 2016 ISSO NÃO FARÁ DIFERENÇA
-  //Não se preocupe, o código irá ativar e desativar esta opção automaticamente.
-  //Caso queira, em 2017, é só tirar o if.
+  // And finally, check if the number is truly valid. Until 2016, a cell phone can
+  // have 8 digits, after that only landline numbers and radios (e.g., Nextel)
+  // can have 8 digits (excluding the area code). So, this function will be inactive
+  // until the end of 2016, and if ANATEL really fulfills the agreement, the numbers will
+  // be correctly validated after that period.
+  // I DID NOT ADD THE VALIDATION OF WHICH STATES HAVE NINTH DIGIT, BECAUSE AFTER 2016 THIS WILL NOT MAKE A DIFFERENCE
+  // Don't worry, the code will enable and disable this option automatically.
+  // If you want, in 2017, just remove the if statement.
   if (new Date().getFullYear() < 2017) return true;
   if (
-    telefone.length == 10 &&
-    [2, 3, 4, 5, 7].indexOf(parseInt(telefone.substring(2, 3))) == -1
+    phoneNumber.length === 10 &&
+    [2, 3, 4, 5, 7].indexOf(parseInt(phoneNumber.substring(2, 3))) === -1
   )
     return false;
 
-  //se passar por todas as validações acima, então está tudo certo
+  // If it passes all the above validations, then everything is fine
   return true;
 }
