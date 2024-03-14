701f4c05f225228a0289c669a403bc29c6c7d27c
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-03-14 14:55:04 -0300

    fix: build

--- src/app/(landing-page)/components/PartnersSlider.tsx
+++ src/app/(landing-page)/components/PartnersSlider.tsx
@@ -1,7 +1,5 @@
 import { ButtonPrimary } from "@/components";
-import Button from "@/components/atoms/Button/Button";
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 import Slider from "react-slick";
 
