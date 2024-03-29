ca5b7ff2f49ae88a0ce71b58802a18d48b9742bc
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-03-28 21:52:55 -0300

    fix: util time remaning

--- src/utils/getDiffInDays.ts
+++ src/utils/getDiffInDays.ts
@@ -3,10 +3,10 @@ import "dayjs/locale/pt-br";
 
 const getDateDiffInDays = (date: string) => {
   const today = dayjs().locale("pt-br");
-  const validity = dayjs(date).locale("pt-br").add(3, "hours");
+  const validity = dayjs(date).locale("pt-br");
 
   const dateDiff = validity.diff(today, "day");
-  const res = dateDiff + 1;
+  const res = dateDiff;
   return res;
 };
 
--- src/utils/getDiffInHours.ts
+++ src/utils/getDiffInHours.ts
@@ -3,7 +3,7 @@ import "dayjs/locale/pt-br";
 
 const getDateDiffInHours = (date: string) => {
   const today = dayjs().locale("pt-br");
-  const validity = dayjs(date).locale("pt-br").add(3, "hours");
+  const validity = dayjs(date).locale("pt-br");
 
   const HoursDiff = validity.diff(today, "hour");
   const res = HoursDiff;
