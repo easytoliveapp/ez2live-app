3c45c3f6a9d1cb0bf2d7dd59048d182cf02e052f
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-04-22 21:39:15 -0300

    feat: improv layout oh the pix component

--- src/components/mols/PixPayment/index.tsx
+++ src/components/mols/PixPayment/index.tsx
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import { Input, ButtonPrimary, FormItem } from "@/components";
 import * as Yup from "yup";
+import Image from "next/image";
+import PixImage from "@/images/easytolive/payment/pix-image.svg";
 
 const PixPayment = () => {
   const [loading, setLoading] = useState(false);
 
   const PixPaymentValidationSchema = Yup.object().shape({
-    name: Yup.string().required("Campo nome é requerido"),
+    cpf: Yup.string().required("Digite seu CPF"),
+    TermsOfUse: Yup.boolean()
+      .required()
+      .oneOf(
+        [true],
+        "Você precisa concordar com os termos de uso para prosseguir.",
+      ),
   });
 
   const handleSubmit = async (values: any) => {
@@ -18,6 +26,7 @@ const PixPayment = () => {
 
   const initialValues = {
     cpf: "",
+    TermsOfUse: false,
   };
   return (
     <Formik
@@ -27,6 +36,7 @@ const PixPayment = () => {
     >
       {({ errors, touched, handleSubmit }) => (
         <Form onSubmit={handleSubmit}>
+          <Image alt="Pix Image" src={PixImage} width={82} height={33} />
           <FormItem
             label="Nome"
             errorMessage={errors.cpf}
@@ -40,9 +50,41 @@ const PixPayment = () => {
               component={Input}
             />
           </FormItem>
+          <p>
+            Pague direto da sua conta, de forma rápida e segura. O PIX permite
+            transferência de dinheiro entre contas sem limite de horário, 24
+            horas por dia, 7 dias por semana.
+          </p>
+          <p>
+            Sua assinatura será confirmada após a identificação da transferência
+            pelo nosso sistema.
+          </p>
+          <div className="flex gap-2">
+            <FormItem
+              label="TermsOfUse"
+              errorMessage={errors.TermsOfUse}
+              invalid={!!(errors.TermsOfUse && touched.TermsOfUse)}
+            >
+              <Field></Field>
+            </FormItem>
+            <input
+              type="checkbox"
+              name="TermsOfUse"
+              id="TermsOfUse"
+              className="bg-generic-alertGreen"
+            />
+            <label htmlFor="TermsOfUse">
+              Ao realizar a assinatura você concorda com os Termos de Uso
+            </label>
+          </div>
+
           <ButtonPrimary loading={loading} disabled={loading}>
-            Gerar QR Code
+            Efetuar Pagamento
           </ButtonPrimary>
+          <span className="text-xs">
+            Os pagamentos serão realizados de forma recorrente a cada renovoção.
+            Você poderá cancelar a qualquer momento em seu perfil.
+          </span>
         </Form>
       )}
     </Formik>
