e12719fe703e978f9a7944ebc61fe86e07378db2
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-05-09 10:45:45 -0300

    fix: export getsubscriptionPageUrl

--- src/utils/getSubscriptionPageUrl.ts
+++ src/utils/getSubscriptionPageUrl.ts
@@ -1,6 +1,9 @@
 import { Route } from "next";
 
-export function getSubscriptionPageURL(supplierId: string, couponId: string) {
+export default function getSubscriptionPageURL(
+  supplierId: string,
+  couponId: string,
+) {
   return `/app/pagamento?callbackUrl=${encodeURIComponent(
     `/app/parceiro/${supplierId}/?coupon=${couponId}`,
   )}` as Route;
