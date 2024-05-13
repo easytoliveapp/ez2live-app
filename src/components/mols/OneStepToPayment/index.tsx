65ddeb96acf3cb9a39af8fab41850414e9542dc4
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-05-13 17:34:32 -0300

    fix: text padding in oneStepPayment Card

--- src/components/mols/OneStepToPayment/index.tsx
+++ src/components/mols/OneStepToPayment/index.tsx
@@ -10,10 +10,10 @@ const OneStepToPayment = () => {
       <p className="text-lg font-bold text-center text-white rounded-full px-5 py-1 bg-generic-limeGreen">
         Você está há 1 passo de <br /> economizar ainda mais!
       </p>
-      <p className="text-center">
-        Seja um assinante{" "}
+      <p className="text-center px-14">
+        Seja um assinante
         <strong className="text-primary-main">EasyToLive</strong> e tenha acesso
-        aos <strong> melhores descontos</strong> em nosso app.
+        aos <strong>melhores descontos</strong> em nosso app.
       </p>
       <span className="text-xs">
         <span className="text-2xl font-bold">R$ 29,90</span>/mês
