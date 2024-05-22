ca98b6df701ba738efb522a8cf03a794e7ef2f33
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-05-22 14:38:20 -0300

    first: UI

---
+++ src/components/mols/TrialEnded/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+import { ButtonPrimary } from "@/components/atoms";
+
+const TrialEnded = () => {
+  return (
+    <div className="flex flex-col justify-center items-center text-center space-y-5">
+      <Image src={} alt="easy-logo" />
+      <h2 className="font-black text-base">
+        Você está há 1 passo de economizar ainda mais!
+      </h2>
+      <span className="text-generic-alertRed font-bold text-sm">
+        {"Seu período de testes acabou :("}
+      </span>
+      <p>
+        Agora você pode tornar-se assinante{" "}
+        <strong className="text-primary-main font-medium">EasyToLive</strong> e
+        continue tendo acesso aos <strong>melhores descontos</strong> em nosso
+        app
+      </p>
+
+      <ButtonPrimary href="/app/pagamento" className="!text-xs">
+        {" "}
+        Quero os melhores descontos
+      </ButtonPrimary>
+    </div>
+  );
+};
+
+export default TrialEnded;
