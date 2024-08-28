393a5f4fc3c8580a3b0f5d08d77ee03c6509889a
Átila de Freitas <atila.jcdefreitas@protonmail.com>
2024-08-28 18:20:16 -0300

    linting

--- src/components/atoms/CategoryCard/index.tsx
+++ src/components/atoms/CategoryCard/index.tsx
@@ -4,36 +4,36 @@ import classNames from "@/utils/classNames";
 import { CATEGORIES } from "@/constants/categories";
 
 export interface ICategorieProps {
-	active: boolean;
-	title: string;
-	id: string;
+  active: boolean;
+  title: string;
+  id: string;
 }
 
 interface ICategoryProps {
-	image: string;
-	id: string;
-	isActive: boolean;
-	name: string;
-	onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  image: string;
+  id: string;
+  isActive: boolean;
+  name: string;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 const CategoryCard: FC<ICategoryProps> = ({ isActive, name, onClick, id }) => {
-	const { image, altImage } = CATEGORIES[id as keyof typeof CATEGORIES] || {
-		image: "",
-	};
+  const { image, altImage } = CATEGORIES[id as keyof typeof CATEGORIES] || {
+    image: "",
+  };
 
-	return (
-		<div
-			className={classNames(
-				"bg-white border-2 focus:border-secondary-main active:shadow-sm rounded-lg p-1 py-2 min-w-[80px] w-full flex flex-col gap-1 items-center cursor-pointer",
-				isActive ? "border-secondary-main" : "border-transparent",
-			)}
-			onClick={onClick}
-		>
-			{image && <Image className="w-8 h-8" src={image} alt={altImage} />}
-			<p className="text-[10px] font-semibold">{name}</p>
-		</div>
-	);
+  return (
+    <div
+      className={classNames(
+        "bg-white border-2 focus:border-secondary-main active:shadow-sm rounded-lg p-1 py-2 min-w-[80px] w-full flex flex-col gap-1 items-center cursor-pointer",
+        isActive ? "border-secondary-main" : "border-transparent",
+      )}
+      onClick={onClick}
+    >
+      {image && <Image className="w-8 h-8" src={image} alt={altImage} />}
+      <p className="text-[10px] font-semibold">{name}</p>
+    </div>
+  );
 };
 
 export default CategoryCard;
